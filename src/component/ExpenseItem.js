40f3ce5cab3d57a277bd51da05b7903fc81fb42d
Arif Hossain <hossaininfo@ymail.com>
2023-10-18 23:18:13 +0200

    added some dumy data to ExpenseItem.js component and added some css styling

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -2,8 +2,12 @@ import './ExpenseItem.css';
 
 function ExpenseItem() {
     return(
-        <div>
-            <p>Expense component</p>
+        <div className="expense-item">
+            <div>Wed 18 Oct 2023</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">456.45 kr.</div>
+            </div>
         </div>
     )
 }
